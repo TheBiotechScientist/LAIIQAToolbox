ca6b2efe767c9510491a91f5d04dd5cde5e867b6
F. Javier Morales M <fmoralesm87@gmail.com>
2022-11-08 20:51:18 -0600

    Eliminación de archivos extras

--- openfile.m
+++
@@ -1,9 +0,0 @@
-function data = openfile()
-
-  [file, pathfile] = uigetfile({'*.mat'},'Seleccionar archivo', 'MultiSelect', 'off');
-
-  data = importdata(fullfile(pathfile, file));
-
-  plot(data(1,:),data(2,:))
-
-end
