8e7514d0b53b0b80a80fcf391e78518c49f4cbe6
F. Javier Morales M <fmoralesm87@gmail.com>
2022-11-10 16:14:49 -0600

    Agregada función saveplot, cambio de datacell a dataraw.

--- laiiqatoolboxclass.m
+++ laiiqatoolboxclass.m
@@ -7,8 +7,8 @@ classdef laiiqatoolboxclass < handle
     %   de Investigación en Ingeniería Química Ambiental (LAIIQA)
     %   de ESIQIE - IPN.
 
-    properties
-        datacell;
+    properties (Access = public)
+        dataraw;
         datacutted;
         title = "Cinética de Ozonización";
         xlabel = 'min';
@@ -18,8 +18,13 @@ classdef laiiqatoolboxclass < handle
         legendFontSize = 8;
         legendLocation = 'best';
         Interpreter = 'tex';
-        plotfig;
+        Resolution = 300;
+    end
 
+    properties (Access = private)
+        plotfig;
+        fig = figure('visible','off');
+        ax = handle(axes);
     end
 
     methods
@@ -28,7 +33,10 @@ classdef laiiqatoolboxclass < handle
         end
 
         function obj = openfiles(obj)
-            clear datacell
+            clear obj.dataraw
+            clear obj.datacutted
+            clear obj.legend
+            clear file pathfile
             [file, pathfile] = uigetfile({'*.mat'},'Seleccionar archivo', 'MultiSelect', 'on');
             if isequal(file,0)
                 disp('No seleccionó ningun archivo.');
@@ -40,7 +48,7 @@ classdef laiiqatoolboxclass < handle
                   obj.legend = file;
                 end
                 for i = 1:length(file)
-                  obj.datacell{i} = importdata(fullfile(pathfile, file{i}));
+                  obj.dataraw{i} = importdata(fullfile(pathfile, file{i}));
                     obj.legend{i} = erase(file{i},'.mat');
                     obj.legend{i} = string(obj.legend{i});
                     % obj.legend{i} = replace(lgnd{i},'_','-');
@@ -49,7 +57,7 @@ classdef laiiqatoolboxclass < handle
         end
 
         function obj = plotfiles(obj)
-            obj.datacutted = obj.datacell;
+            obj.datacutted = obj.dataraw;
             if obj.xlabel == 'min'
                 t = 60;
             elseif obj.xlabel == 'h'
@@ -57,15 +65,15 @@ classdef laiiqatoolboxclass < handle
             elseif obj.xlabel == 'seg'
                 t = 1;
             end
-            fig = figure;
-            ax = axes;
+            obj.fig.Visible = 'on';% = figure;
+            obj.ax = axes;% = axes;
             for i=1:length(obj.datacutted)
                 obj.datacutted{i}(1,:) = obj.datacutted{i}(1,:)/t;
                 obj.datacutted{i} = obj.datacutted{i}(:,find(obj.datacutted{i}(1,:)==60/t):end);
                 obj.datacutted{i} = obj.datacutted{i}(:,find(obj.datacutted{i}(2,:)==min(obj.datacutted{i}(2,1:find(obj.datacutted{i}(1,:)==10*60/t)))):end);
                 obj.datacutted{i}(1,:) = obj.datacutted{i}(1,:)-obj.datacutted{i}(1,1);
-                hold(ax,'on');
-                obj.plotfig = plot(ax, obj.datacutted{i}(1,:),obj.datacutted{i}(2,:))
+                hold(obj.ax,'on');
+                obj.plotfig(i) = plot(obj.ax, obj.datacutted{i}(1,:),obj.datacutted{i}(2,:))
                 title(obj.title);
                 xlabel("Tiempo (" + obj.xlabel + ")");
                 ylabel(obj.ylabel);
@@ -74,13 +82,42 @@ classdef laiiqatoolboxclass < handle
                 else
                   legend(obj.legend,'FontSize',obj.legendFontSize,'Location',obj.legendLocation,'Interpreter',obj.Interpreter);
                 end
-                hold(ax,'off');
+                hold(obj.ax,'off');
             end
         end
 
         function obj = set(obj,title)
             obj.title = title;
             obj.plotfiles(obj);
+        end
+
+        function obj = saveplot(obj,name)
+            obj.fig;
+            obj.ax = axes;% = axes; % Checar como limpiar la figura
+            hold(obj.ax,'on');
+            for i=1:length(obj.datacutted)
+                plot(obj.ax,obj.datacutted{i}(1,:),obj.datacutted{i}(2,:));
+                title(obj.title);
+                xlabel("Tiempo (" + obj.xlabel + ")");
+                ylabel(obj.ylabel);
+                grid(obj.grid);
+                if isempty(obj.legend)
+                else
+                  legend(obj.legend,'FontSize',obj.legendFontSize,'Location',obj.legendLocation,'Interpreter',obj.Interpreter);
+                end
+            end
+            hold(obj.ax,'off');
+            if contains(name,'.pdf')
+                exportgraphics(obj.fig,name,'ContentType','vector');
+            elseif contains(name,'.png') | contains(name,'.jpg') | contains(name,'.jpeg')
+                exportgraphics(obj.fig,name,'Resolution',obj.Resolution);
+            elseif contains(name,'.fig')
+                savefig(obj.fig,name);
+            elseif contains(name,'.svg') | contains(name,'.eps')
+                saveas(obj.fig,name)
+            else
+                disp("Especifica un formato válido: .png, .jpg, .jpeg, .pdf, .svg, .fig. Ejemp.: 'mifigura.pdf'");
+            end
         end
     end
 end
